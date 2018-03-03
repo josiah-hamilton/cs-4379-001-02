935c4d47e48f2fe966d3044ecdfed1327daa0c24
flashcookies <flashcookies@github.com>
2018-03-02 22:45:42 -0600

    Updated information for future use in Makefile. Completed Homework 2

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC=mpicc
 %.o:%.c
 	$(CC) $(CFLAGS) $< -o $@
 hwk2make: main.c
-	#module load gnu openmpi # Module not found in PATH
+	#module load gnu openmpi # module is a function echo $(type module)
 	$(CC) -o hwk2 main.c
 clean:
 	rm hwk2
--- main.c
+++ main.c
@@ -15,34 +15,33 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    //#ifdef DEBUG
-	////sleep a certain amount of s (offset based on ranks to demonstrate sync)
-    //int offset = rank+1;
-	//int s = 0;
-	//for(int i = 0; i < 500000000*offset; i++){
-	//	s = i;
-	//}
-    //#endif
+    #ifdef DEBUG
+	//sleep a certain amount of s (offset based on ranks to demonstrate sync)
+    int offset = rank+1;
+	int s = 0;
+	for(int i = 0; i < 500000000*offset; i++){
+		s = i;
+	}
+    #endif
 
     int data = 1;
     MPI_Status status;
-    //implement broadcasting
     int step;
 
-    // BROAD RECEIVE
+    // BROADRECEIVE
 
     for(step = ranks>>1; step >= 1; step = step >> 1) {
         if(rank < step) { 
 #ifdef DEBUG
-//            printf("%d:\t here0 %d\t Recv? = %d\t Tag = %d\n", 
-//                    rank, step, rank+step, rank);
+            printf("%d:\t here0 %d\t Recv? = %d\t Tag = %d\n", 
+                    rank, step, rank+step, rank);
 #endif
 	        MPI_Recv(&data, 1, MPI_INT, rank+step, rank, MPI_COMM_WORLD, &status);
         } else if (rank < 2*step) {
 	        MPI_Send(&data, 1, MPI_INT, rank-step, rank-step, MPI_COMM_WORLD);
 #ifdef DEBUG
-//            printf("%d:\t here0 %d\t Send? = %d\t Tag = %d\n", 
-//                    rank, step, rank-step, rank-step);
+            printf("%d:\t here0 %d\t Send? = %d\t Tag = %d\n", 
+                    rank, step, rank-step, rank-step);
 #endif
         } 
     }   
@@ -52,19 +51,22 @@ int main(int argc, char** argv) {
     printf("%d: Broadcast\n", rank);
 #endif
     
-    for(step = 1; step < ranks; step = step >> 1){
+    for(step = 1; step < ranks; step = step << 1){
+#ifdef DEBUG 
+        printf("%d:\tStepBroadcast: %d\n", rank, step);
+#endif
         if(rank < step) {
+            MPI_Send(&data, 1, MPI_INT, rank+step, rank+step, MPI_COMM_WORLD);
 #ifdef DEBUG
             printf("%d:\t here1 %d\t Send = %d\t Tag = %d\n", 
                     rank, step, rank+step, rank+step);
 #endif
-            MPI_Send(&data, 1, MPI_INT, rank+step, rank+step, MPI_COMM_WORLD);
         } else if(rank < 2*step) {
+            MPI_Recv(&data, 1, MPI_INT, rank-step, rank, MPI_COMM_WORLD, &status);
 #ifdef DEBUG
             printf("%d:\t here1 %d\t Recv = %d\t Tag = %d\n", 
                     rank, step, rank-step, rank);
 #endif
-            MPI_Recv(&data, 1, MPI_INT, rank, rank, MPI_COMM_WORLD, &status);
         }
     }
 
